a5ffff37f5122124f6e51d1a10f4957ebca9e3dc
Scott Bollinger <bollsc@gmail.com>
2015-02-21 23:33:20 -0500

    Initial GUI for saving markers to db
    Created ugly initial GUI for getting marker info and created php file to
    save it to the db. Also created main plugin file that contains necessary
    WordPress comments and the "hook" to start the plugin.

---
+++ jof-map-plugin/journey-of-faith-map.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Journey of Faith Map
+* Description: Plugin to add an interactive map for the JoF organization
+* Version: 1.0
+* Author: Nathan Jackson, Scott Bollinger, Dominic Desimio, Jordan Cordova, Jacob Slezak
+* License: MIT
+*/
+
+register_activation_hook(/*file*/, /*function to call*/);
