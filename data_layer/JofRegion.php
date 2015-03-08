d4f0d83a515298eb51ac2bb3c89c07ba24cef798
Nathan Jackson <nate.ds.jackson@gmail.com>
2015-03-07 19:54:10 -0500

    Added region class and database interface.

---
+++ data_layer/JofRegion.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+* Class representing a Journey of Faith region.
+*/
+class  JofRegion {
+	private $regionId;
+	private $name;
+	private $geoJsonStr;
+
+	/**
+	* Construct a new JofRegion object.
+	*/
+	public function __construct($name, $geoJsonStr) {
+		$this->regionId = null;
+		$this->name = $name;
+		$this->geoJsonStr = $geoJsonStr;
+	}
+
+	/**
+	* Returns the region id.  Null if this object didn't come from the
+	* database.
+	*/
+	public function getRegionId() {
+		return $this->regionId;
+	}
+
+	/**
+	* Returns the region name.
+	*/
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	* Returns the GeoJSON representation of the region.
+	*/
+	public function getGeoJsonStr() {
+		return $this->geoJsonStr;
+	}
+
+	/**
+	* Sets the region id.
+	*/
+	public function setRegionId($regionId) {
+		$this->regionId = $regionId;
+	}
+
+	/**
+	* Sets the region name.
+	*/
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	/**
+	* Sets the region's GeoJSON representation.
+	*/
+	public function setGeoJsonStr($geoJsonStr) {
+		$this->geoJsonStr = $geoJsonStr;
+	}
+}
