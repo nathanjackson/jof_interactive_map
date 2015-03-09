f98aa4781c9d8a87d1df8b4b547b46ad57856b36
Scott Bollinger <bollsc@gmail.com>
2015-03-09 16:28:31 -0400

    added main menu
    using memberinterface to add members from marker info. created main
    menu. got rid of some duplicate files.

--- jof-map-plugin/journey-of-faith-map.php
+++ jof-map-plugin/journey-of-faith-map.php
@@ -7,4 +7,39 @@
 * License: MIT
 */
 
-register_activation_hook(/*file*/, /*function to call*/);
+/**
+* Adds map data tables to the Wordpress database.
+*/
+function install() {
+	global $wpdb;
+
+	$charset_collate = $wpdb->get_charset_collate();
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+	// Add JofMembers table.
+	$tbl = $wpdb->prefix . "jofmembers";
+	$sql = "CREATE TABLE `" . $tbl . "` (
+		`memberid` int(11) NOT NULL AUTO_INCREMENT,
+		`title` varchar(1024) NOT NULL,
+		`address` varchar(1024) NOT NULL,
+		`email` varchar(1024) NOT NULL,
+		`skills` text NOT NULL,
+		PRIMARY KEY  (`memberid`)
+	) " . $charset_collate . ";";
+	dbDelta($sql);
+
+	// Add JofRegions table.
+	$tbl = $wpdb->prefix . "jofregions";
+	$sql = "CREATE TABLE `" . $tbl . "` (
+		`regionid` int(11) NOT NULL AUTO_INCREMENT,
+		`name` varchar(1024) NOT NULL,
+		`geojsonstr` text NOT NULL,
+		PRIMARY KEY  (`regionid`)
+	) " . $charset_collate . ";";
+	dbDelta($sql);
+}
+
+register_activation_hook(__FILE__, 'install');
+?>
+
--- jof-map-plugin/markers/marker.php
+++ jof-map-plugin/markers/marker.php
@@ -4,31 +4,17 @@
 	<!--Use google geolocation api to turn address into lat,long
 	save info to database-->
 	<?php
-		$servername = "";
-		$username = "";
-		$password = "";
+		include_once('data_layer/JofMember.php');
+		include_once('data_layer/JofMembersInterface.php');
 		
-		$connection = new mysqli($servername, $username, $password);
-		if ($conn->connect_error) 
-		{
-			die("Connection failed: " . $conn->connect_error);
-		} 
+		$memberId = $_POST["name"];
+		$title = $_POST["title"];
+		$address = $_POST["address"];
+		$email = $_POST["contact"];
+		$skills = $_POST["specialty"];
 		
-		$sql = $connection->prepare("INSERT INTO Markers (address, latitude, longitude, title, icon, name, specialty, contact)
-		VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-		$sql->bind_param("ssssssss", $_POST["address"], $latitude, $longitude, $_POST["title"], $_POST["icon"], $_POST["name"], $_POST["specialty"], $_POST["contact"]);
-		$sql->execute();
-		
-		if ($conn->query($sql) === TRUE) 
-		{
-			echo "New record created successfully";
-		}	
-		else 
-		{
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-		$sql->close();
-		$conn->close();
+		$member = new JofMember($title, $address, $email, $skills);
+		addMemberToDatabase($member);
 	?>
 </body>
 </html>
